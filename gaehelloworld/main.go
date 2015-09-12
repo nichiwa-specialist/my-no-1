2cb612161303b8fcbf94d34b4aeaeb1bc4ce829e
unknown <m-kuwahara@TEC202.nichiwa2.local>
2015-09-12 16:43:02 +0900

    一覧取得の再実装と、詳細取得の固定結果出力実装

--- gaehelloworld/main.go
+++ gaehelloworld/main.go
@@ -4,7 +4,30 @@ import (
 	"github.com/ant0ine/go-json-rest/rest"
 	"log"
 	"net/http"
+	"strconv"
+	"fmt"
 )
+
+// ヘッダー情報構造体 for json
+type List struct {
+	Key int				`json:"key"`			// 後でデータベースのキーに変更 *datastore.Key
+	Title string		`json:"title"`			// タイトル
+	Lat float32			`json:"lat"`			// 緯度
+	Lng float32			`json:"lng"`			// 経度
+	Adr string			`json:"adr"`			// 住所
+	Date string			`json:"date"`			// 後でデータベースの日付に変更 time.Time 
+}
+
+// 詳細情報構造体 for json
+type Detail struct {
+	Key int				`json:"key"`			// 後でデータベースのキーに変更 *datastore.Key
+	Title string		`json:"title"`			// タイトル
+	Lat float32			`json:"lat"`			// 緯度
+	Lng float32			`json:"lng"`			// 経度
+	Adr string			`json:"adr"`			// 住所
+	Date string			`json:"date"`			// 後でデータベースの日付に変更 time.Time
+	Detail string		`json:"detail"`			// 本文
+}
 
 func init() {
 	api := rest.NewApi()
@@ -13,7 +36,37 @@ func init() {
 		rest.Get("/message", func(w rest.ResponseWriter, req *rest.Request) {
 			w.WriteJson(map[string]string{"Body": "Hello World!"})
 		}),
-	)
+		rest.Get("/article", func(w rest.ResponseWriter, req *rest.Request) {
+		
+			var l []List
+
+			l = append(l, List{Title: "世界一うまいラーメン", Lat: 35.0394195, Lng: 135.7915279, Adr: "大阪" } )
+			l = append(l, List{Title: "Test2", Adr: "兵庫" } )
+		
+			w.WriteJson(l)
+		}),
+		rest.Get("/article/:key", func(w rest.ResponseWriter, req *rest.Request) {
+
+			var l Detail
+//			var intKey int
+
+//			var strKey string
+//			strKey = req.PathParam("key")
+			intKey, err := strconv.ParseInt( req.PathParam("key"), 10 , 0 )
+
+			if err != nil{
+			  fmt.Println(err)
+			}
+			
+			l.Key = int(intKey)
+			l.Title = "世界一うまいラーメン"
+			l.Lat = 35.0394195
+			l.Lng = 135.7915279
+			l.Detail = "詳細"
+
+			w.WriteJson(l)
+		}),
+		)
 	if err != nil {
 		log.Fatal(err)
 	}
