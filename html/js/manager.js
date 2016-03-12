a9a436ab1c77a7cddc1d44500995e23e5c1f0dc3
s-nakano-nichiwa <s-nakano@nichiwa-system.co.jp>
2016-03-12 16:53:43 +0900

    POST機能実装。

--- html/js/manager.js
+++ html/js/manager.js
@@ -5,4 +5,33 @@ $(document).ready(function() {
 
 	$('select').material_select();
 	
+	$("#post").on("touchend mouseup", function() {
+		var url = "/article/";
+		
+		var data = {
+			"title" : $("#title").val(),
+			"lat" : "35.039419",
+			"lng" : "135.7915279",
+			"detail" : $("#detail").val()
+		};
+		
+		console.log(JSON.stringify(data));
+		
+		$.ajax({
+			type: "POST",
+			url: url,
+			data : JSON.stringify(data),
+			contentType: 'application/JSON',
+			dataType : 'JSON',
+			scriptCharset: 'utf-8',
+			success: function(data) {
+				alert("投稿が完了しました");
+			},
+			error: function(data) {
+				console.log(data);
+				alert("投稿が失敗しました");
+			}
+		});
+	});
+	
 });
